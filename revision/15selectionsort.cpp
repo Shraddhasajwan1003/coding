7ea28c1da850f5d9828340cfb876b04d087b88df
SHRADDHA SAJWAN <shraddhasajwan2004@gmail.com>
2026-07-01 09:05:48 +0530

    DSA notes 1-07-26

--- revision/15selectionsort.cpp
+++ revision/15selectionsort.cpp
@@ -1,48 +1,35 @@
 /*iteration 1 : mini nikalo array ka , then we will swap that first element elemnet we started iterating with , with the mini , mini or first ko swap krdo , ab hmara first index yani idx=0 is sorted , then next iteration 2, usme start from index =1 , therefore idx++ hua hoga prevoius step me , then again find mini ,swap it with starting index in this case idx=1 and so on after n-1 iterations array is sorted  */
 #include <bits/stdc++.h>
 using namespace std;
-
-// Function to perform selection sort
-void selection_sort(int arr[], int n) {
-    // Traverse the array
-    for (int i = 0; i < n - 1; i++) {
-        int mini = i; // Assume current index holds the minimum value
-
-        // Find the index of the smallest element in the remaining array
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[mini]) {
-                mini = j; // Update mini if smaller element is found
+//TC is O(N^2)
+class Solution {
+public:
+    void selectionsort (int arr[], int n ){//take array and size of array as input 
+        for ( int i =0; i<n-1;i++){//Writing it as i < n and j = i is not "wrong" because it doesn't break the sort, but it is inefficient.
+            //by taking n-1 we are avoiding extra iteration and by j=i+1 avoids one extra comparison that is present if j=i
+            int mini =i;// take index as minimum 
+            for( int j=i+1;j<n-1;j++){
+                if ( arr[j]<arr[mini]){//check if any index j has value lesser than value on mini index
+                    mini =j;//value if index of j is less than that of mini then update mini with j  
+                }
             }
+            swap(arr[i],arr[mini]);//swap the mini and index we assumed to be mini at first, this way upto index i is sorted 
         }
-
-        // Swap the found minimum element with the first element of unsorted part
-        int temp = arr[mini];
-        arr[mini] = arr[i];
-        arr[i] = temp;
+        for( int i =0;i<n;i++){//print sorted array 
+            cout<<arr[i]<<" ";
+        }
     }
-
-    // Print the sorted array
-    cout << "After selection sort: " << "\n";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << "\n";
-}
+};
 
 int main() {
-    // Initialize array
-    int arr[] = {13, 46, 24, 52, 20, 9};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    // Print array before sorting
-    cout << "Before selection sort: " << "\n";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    Solution obj;
+    int n ;
+    cin>>n;
+    int arr[n];
+    for( int i =0;i<n;i++){
+        cin>>arr[i];
     }
-    cout << "\n";
-
-    // Call selection sort
-    selection_sort(arr, n);
-
+    n=sizeof(arr)/sizeof(arr[0]);
+    obj.selectionsort(arr,n);
     return 0;
 }
